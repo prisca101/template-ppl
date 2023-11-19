fdd9b8b211905d5f22e656d9734f8b05ba48da2e
prisca101 <priscadella.18@gmail.com>
2023-11-20 02:10:21 +0700

    update

--- resources/views/operator/mahasiswa.blade.php
+++ resources/views/operator/mahasiswa.blade.php
@@ -33,7 +33,7 @@
 @endsection
 
 @section('content2')
-    <div class="sm:flex">
+    <div class="sm:flex mb-4">
         <div class="items-center hidden mb-3 sm:flex sm:divide-x sm:divide-gray-100 sm:mb-0 dark:divide-gray-700">
             <form class="lg:pr-3" action="#" method="GET">
                 <label for="users-search" class="sr-only">Search</label>
@@ -77,7 +77,7 @@
     </div>
 
     <div
-        class="pt-4 bg-white block sm:flex items-center justify-between border-b border-gray-200 lg:mt-1.5 dark:bg-gray-800 dark:border-gray-700">
+        class="bg-white block sm:flex items-center justify-between border-b border-gray-200 lg:mt-1.5 dark:bg-gray-800 dark:border-gray-700">
         <div class="w-full mb-1">
             <div class="flex flex-col">
                 <div class="overflow-x-auto">
@@ -513,28 +513,6 @@
             <span class="text-sm font-normal text-gray-500 dark:text-gray-400">Showing <span
                     class="font-semibold text-gray-900 dark:text-white">1-20</span> of <span
                     class="font-semibold text-gray-900 dark:text-white">2290</span></span>
-        </div>
-        <div class="flex items-center space-x-3">
-            <a href="#"
-                class="inline-flex items-center justify-center flex-1 px-3 py-2 text-sm font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
-                <svg class="w-5 h-5 mr-1 -ml-1"" fill="currentColor" viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg">
-                    <path fill-rule="evenodd"
-                        d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
-                        clip-rule="evenodd"></path>
-                </svg>
-                Previous
-            </a>
-            <a href="#"
-                class="inline-flex items-center justify-center flex-1 px-3 py-2 text-sm font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
-                Next
-                <svg class="w-5 h-5 ml-1 -mr-1" fill="currentColor" viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg">
-                    <path fill-rule="evenodd"
-                        d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                        clip-rule="evenodd"></path>
-                </svg>
-            </a>
         </div>
     </div>
 
