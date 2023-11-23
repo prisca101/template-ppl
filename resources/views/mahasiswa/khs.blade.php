f25b63a1d7a335b2e46a7799cbe8cdc53b7b2f07
prisca101 <priscadella.18@gmail.com>
2023-11-23 20:01:40 +0700

    update

--- resources/views/mahasiswa/khs.blade.php
+++ resources/views/mahasiswa/khs.blade.php
@@ -236,8 +236,12 @@
                                             </td>
                                             <td class="p-4 whitespace-nowrap">
                                                 <span
-                                                    class="bg-green-100 text-green-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded-md dark:bg-gray-700 dark:text-green-400 border border-green-100 dark:border-green-500">{{ $khs->status }}</span>
-                                            </td>
+                                                    class="@if($khs->status == 'lulus') bg-green-100 text-green-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded-md dark:bg-gray-700 dark:text-green-400 border border-green-100 dark:border-green-500
+                                                    @elseif ($khs->status == 'pending') bg-orange-100 text-orange-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded-md border border-orange-100 dark:bg-gray-700 dark:border-orange-300 dark:text-orange-300
+                                                    @else bg-red-100 text-red-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded-md border border-red-100 dark:border-red-400 dark:bg-gray-700 dark:text-red-400
+                                                    @endif">{{ $khs->status }}
+                                                </span>
+                                            </td>                                            
                                         </tr>
                                     @endforeach
                                 </tbody>
