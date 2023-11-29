54b1870da75578278ac529f59b65791c3f7a2134
Apasha99 <elisakds88@gmail.com>
2023-11-29 21:17:00 +0700

    oo

--- resources/views/mahasiswa/profil-edit2.blade.php
+++ resources/views/mahasiswa/profil-edit2.blade.php
@@ -49,7 +49,7 @@
                     <div class="flex items-center space-x-4">
                         <div class="relative inline-block">
                             <input type="file" class="absolute inset-0 w-full h-full opacity-0 cursor-pointer" id="foto" name="foto"
-                        accept=".jpg, .jpeg, .png" />
+                        accept=".jpg, .jpeg, .png" required />
                             <button type="button"
                                 class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
                                 Upload file
