6b35f04fafd228b21348e7089226de2fdeae36eb
Apasha99 <elisakds88@gmail.com>
2023-12-07 01:01:06 +0700

    zzzzzzzz

--- resources/views/operator/layouts/layout.blade.php
+++ resources/views/operator/layouts/layout.blade.php
@@ -113,18 +113,6 @@
                         </svg>
                     </button>
 
-                    <button type="button" data-dropdown-toggle="notification-dropdown"
-                        class="p-2 text-gray-500 rounded-lg hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-700">
-                        <span class="sr-only">View notifications</span>
-
-                        <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20"
-                            xmlns="http://www.w3.org/2000/svg">
-                            <path
-                                d="M10 2a6 6 0 00-6 6v3.586l-.707.707A1 1 0 004 14h12a1 1 0 00.707-1.707L16 11.586V8a6 6 0 00-6-6zM10 18a3 3 0 01-3-3h6a3 3 0 01-3 3z">
-                            </path>
-                        </svg>
-                    </button>
-
 
                     <div class="z-20 z-50 hidden max-w-sm my-4 overflow-hidden text-base list-none bg-white divide-y divide-gray-100 rounded shadow-lg dark:bg-gray-700 dark:divide-gray-600"
                         id="apps-dropdown"
